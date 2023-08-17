612904fa3636057f507e245d2a521dbd62a4894d
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-17 09:52:42 +0100

    added some changes

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+// Define Route in express
+// routing means determining how an applicaton responds to a certain client request to a certain URL
+app.get('/', (req, res) => {
+    res.status(200)
+    .json({message: 'Hello from the server side!', app: 'Natours'});
+});
+
+app.post('/', (req, res) => {
+    res.send('You can post to this endpoint...')
+});
+
+
+// starting up a server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on ${port}... `);
+});
+
+
