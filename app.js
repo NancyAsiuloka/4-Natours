d12dad8a11597b01b0e030431540967fdb1bcd83
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-19 14:57:58 +0100

    called the next function

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
     //adding the current time to the request
     req.requestTime = new Date().toISOString;
-    ne
+    next();
 });
 
 
