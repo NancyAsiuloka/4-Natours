0624fa53d7186cd767a72ec05529e13c107607e0
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-19 15:08:30 +0100

    required morgan

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const fs = require('fs');
+
 
 // using this middleware to handle the post request
 app.use(express.json());
