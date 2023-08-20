2d7e1ac783ed24e7556b3de98abb0b8245fe6fda
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-20 13:31:31 +0100

    created file for tourRoutes

---
+++ routes/tourRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const tourController = require('./../controllers/tourController');
+
+
+// 3: ROUTES FOR TOURS
+
+const router = express.Router();
+
+router
+.route('/')
+.get(tourController.getAllTours)
+.post(tourController.createTour);
+
+router
+.route('/:id')
+.get(tourController.getTour)
+.patch(tourController.updateTour)
+.delete(tourController.deleteTour);
+
+module.exports = router;
