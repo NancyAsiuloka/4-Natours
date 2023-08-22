fdb51e7a49354be3cc1e8801382b796cd03ba551
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-22 09:49:56 +0100

    added a comment

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+
 // creating our own Middleware functions
 app.use((req, res, next) => {
     console.log('Hello from the middleware');
