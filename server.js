b7cdc1773fcbb5194505e9c1d3236460fd512da8
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-22 10:02:07 +0100

    added a comment

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const app = require('./app');
 
+// ENVIRONMENT VARIABLES
+// They are variables that are used to define
+// the environment in which a node app is running
+console.log(app.get('env'));
+
 // 4: START SERVER
 const port = 3000;
 app.listen(port, () => {
-  console.log(`App running on ${port}... `);
+  console.log(`App running on  ${port}... `);
 });
 
 /*
