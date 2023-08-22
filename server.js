b2bda8bb66004a9882c048e44ec871941cfa60ce
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-22 10:29:23 +0100

    added  a comment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 const app = require('./app');
 
-
+dotenv.config({ path: './config.env' });
 
 // ENVIRONMENT VARIABLES
 // They are variables that are used to define
