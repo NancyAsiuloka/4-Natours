3c45f5c58faad4ccbd6dc41d6905dc113bbeb8ab
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-26 10:41:14 +0100

    created a new file

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,7 +4,6 @@ const tourController = require('./../controllers/tourController');
 // 3: ROUTES FOR TOURS
 const router = express.Router();
 
-// routes for the check id
 router.param('id', tourController.checkID);
 
 // Create a checkBody middleware
