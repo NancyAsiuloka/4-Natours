f6a6d969aad428cf59963688329e89173c680509
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-29 11:10:45 +0100

    connected the database successfully

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const app = require('./app');
 dotenv.config({ path: './config.env' });
 
 
-const DB = process.env.DATABASE_PASSWORD.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE_URL.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log('DB connected successfully');
