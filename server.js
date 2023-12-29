035c279f046cab50814d67ed88aefca85d597e61
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-30 00:27:30 +0100

    added a schema

--- server.js
+++ server.js
@@ -21,9 +21,19 @@ mongoose
   });
 
   const tourSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
-    price: Number,
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
   })
 
 // ENVIRONMENT VARIABLES
