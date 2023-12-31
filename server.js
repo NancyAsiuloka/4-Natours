37aea53f47816eb475def3f5cb174e6904943546
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-12-31 11:38:44 +0100

    made some changes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // Requiring dot env module
-const mongoose = require('mongoose')
 const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const mongoose = require('mongoose')
+
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
 
 
 const DB = process.env.DATABASE_URL.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
