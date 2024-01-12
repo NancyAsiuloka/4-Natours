4597818ad734af4c66b1f8a23d7a77555d5ee7d0
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-01-12 20:05:19 +0100

    importing data into the database

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,7 +1,8 @@
+const fs =  require('fs');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
-
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const Tour = require('./../../models/tourModel')
 
 const DB = process.env.DATABASE_URL.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
@@ -15,3 +16,15 @@ mongoose
   .catch((err) => {
     console.error('Error connecting to the database:', err.message);
   });
+
+  // READ JSON FILE
+  const tours = fs.readFileSync('tours-simple.json', 'utf-8')
+
+  // IMPORT DATA INTO DB
+  const importData = async () => {
+    try{
+
+    }catch (err){
+      console.log(err);
+    }
+  }
