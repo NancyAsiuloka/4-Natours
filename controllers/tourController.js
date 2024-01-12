5abcf6f6db29c88b6b35e73ad6c33296d3cd509d
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-01-12 21:15:21 +0100

    added a comment

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,17 +5,17 @@ exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
 
-    // const tours = await Tour.find({
-    //   duration: 5,
-    //   difficulty: 'easy'
-    // });
+    const tours = await Tour.find({
+      duration: 5,
+      difficulty: 'easy'
+    });
 
-    // written using special mongoose method
-    const tours = await Tour.find()
-      .where('duration')
-      .equals(5)
-      .where('difficulty')
-      .equals('easy');
+    // // written using special mongoose method
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
 
     res.status(200).json({
       status: 'success',
