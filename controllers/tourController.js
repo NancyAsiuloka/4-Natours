760cdb6bac7acce393c7f7eccc7a79095b31edba
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-01-14 15:03:43 +0100

    refactured the APIFeatures

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,67 +1,24 @@
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures')
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = '-ratingsAverage,price';
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
-}
+};
+
+
 // get all tours
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query);
-    // Build Query
-    // 1a) Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 1b)Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
-
-    // const query = Tour.find(queryObj);
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-     if(req.query.sort){
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-     } else {
-      query = query.sort('-createdAt')
-     }
-
-    // 3) Field Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');  //create the field in the order specified in the db
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v')
-    }
-
-    // 4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    // page=2&limit=10, 1-10=page1, 11-20=page2 ...
-    // calculate d skip according to the page value
-    query = query.skip(skip).limit(limit);
-
-    if(req.query.page){
-      const numTours = await Tour.countDocuments();
-      if(skip >= numTours) throw new Error('This page does not exist')
-    }
-
     // Execute Query
-    const tours = await query;
-
-    // // written using special mongoose method
-    // const query = Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     // Send Response
     res.status(200).json({
