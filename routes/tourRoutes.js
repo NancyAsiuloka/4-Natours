51731de264b1a15d4db190ce98c182e07a438ffa
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-01-14 15:32:43 +0100

    added the getTourStats route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -11,18 +11,24 @@ const router = express.Router();
 // If not, send back 400(bad request)
 // Add it to the post handler stack
 
-router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+  router
+  .route('/tour-stats')
+  .get(tourController.getTourStats);
+
 
 router
-.route('/')
-.get(tourController.getAllTours)
-.post(tourController.createTour);
-
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
 
 router
-.route('/:id')
-.get(tourController.getTour)
-.patch(tourController.updateTour)
-.delete(tourController.deleteTour);
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
