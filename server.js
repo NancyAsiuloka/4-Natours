fef7cfe9773489f7e5fd1caacb3c92f63a3f6272
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-01-17 09:04:52 +0100

    added a function

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // Requiring dot env module
+const mongoose = require('mongoose')
+
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
-const mongoose = require('mongoose')
 const app = require('./app');
 
 const DB = process.env.DATABASE_URL.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
