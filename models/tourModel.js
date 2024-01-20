d8d98bb5d29af66d82c7c2b54603ccafbbe5a6c2
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-01-20 14:55:50 +0100

    added aggregate middleware function

--- models/tourModel.js
+++ models/tourModel.js
@@ -94,10 +94,15 @@ tourSchema.pre(/^find/, function(next){
 
 tourSchema.post(/^find/, function(docs,next){
   console.log(`Query took ${Date.now() - this.start} milliseconds`)
-  console.log(docs);
   next();
 })
 
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({$match: {secretTour: {$ne: true}}})
+  console.log(this)
+  next();
+})
 
 
   const Tour = mongoose.model('Tour', tourSchema);
