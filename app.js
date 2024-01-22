d087f6ee4598f9bd9432376fca925ca06d5ccd15
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-01-22 10:12:10 +0100

    made some changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -36,16 +37,7 @@ app.use('/api/v1/users', userRouter);
 
 // Handling 404 Errors
 app.all('*', (req, res, next) => {
-    // res.status(404).json({
-    //     status: 'fail',
-    //     message: `Can't find ${req.originalUrl} on this server!`
-    // })
-
-    const err = new Error(`Can't find ${req.originalUrl} on this server!`)
-    err.status = 'fail';
-    err.statusCode = 404
-
-    next(err)
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
 })
 
 // Global error handler
