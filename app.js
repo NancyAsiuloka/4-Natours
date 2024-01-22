9b84fd8a526e1edd190eb84ebe4617743c790281
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-01-22 09:50:16 +0100

    added a global error handler

--- app.js
+++ app.js
@@ -36,9 +36,24 @@ app.use('/api/v1/users', userRouter);
 
 // Handling 404 Errors
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server!`
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl} on this server!`
+    // })
+
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`)
+    err.status = 'fail';
+    err.statusCode = 404
+})
+
+// Global error handler
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: 'err.status',
+        message: err.message
     })
 })
 
