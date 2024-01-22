ce83839b84101a7f529c7c0377c6501c2e65f6c1
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-01-22 09:29:08 +0100

    handling 404 errors

--- app.js
+++ app.js
@@ -34,4 +34,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// Handling 404 Errors
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server!`
+    })
+})
+
 module.exports = app;
