1557b2c7f8232ab190f113e2a34fa20a92d2b282
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-01-24 09:12:56 +0100

    made some changes

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 // Requiring dot env module
 const mongoose = require('mongoose')
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...')
+  console.log(err.name, err.message)
+  process.exit(1)
+})
+
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
@@ -26,11 +32,16 @@ mongoose
 
 // 4: START SERVER
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on ${port}... `);
 });
 
 // handling a promise rejection globally
 process.on('unhandledRejection', err => {
   console.log(err.name, err.message)
+  console.log('UNHANDLED REJECTION! Shutting down...')
+  server.close(() => {
+    process.exit(1)
+  })
 })
+
