5255252461ae7cb92d6620e85225c62205db8911
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-01-24 09:45:46 +0100

    created users schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please tell us your name!'],
+    },
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        required: [true, 'Please provide an email'],
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+        minlength: 8,
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate(this) { return this.password === this.passwordConfirm },
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
