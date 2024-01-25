55bed49a99b882c2f774f8fa8d8483d333f11734
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-01-25 21:51:59 +0100

    added a function

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -22,9 +23,27 @@ const userSchema = new mongoose.Schema({
     passwordConfirm: {
         type: String,
         required: [true, 'Please confirm your password'],
-        // validate(this) { return this.password === this.passwordConfirm },
+        validate: {
+            // this only works on save
+            validator: function() {
+                return this.password === this.passwordConfirm;
+            },
+            message: 'Passwords are not the same!'
+        }
     }
 });
+
+userSchema.pre('save', async function(next) {
+//    Only run this fn if password was actually modified
+    if (!this.isModified('password')) return next();
+
+    // Hash the password with cust of 12
+    this.password = await bcrypt.hash(this.password, 12)
+
+    // Delete the passwordConfirm field
+    this.passwordConfirm = undefined;
+    next();
+})
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
