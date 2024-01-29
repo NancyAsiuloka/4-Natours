d196ac1ab0cd455f18f40a95c114840f9db19272
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-01-29 17:51:16 +0100

    made some changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync')
+const AppError = require('./../utils/appError')
 
 exports.signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create({
@@ -22,4 +23,22 @@ exports.signup = catchAsync(async (req, res, next) => {
         }
     })
 });
+
+exports.login =  (req, res, next) => {
+    const {email, password} = req.body;
+
+    //  1)Check if email and password exist
+    if(!email || !password) {
+        return next(new AppError('Please provide email and password!', 400))
+    }
+    // 2) Check if the user exists && password is correct
+
+    // 3) If everything is okay, send the token to the client
+    const token = '';
+    res.status(200).json({
+        status: 'Success',
+        token
+    })
+
+}
 
