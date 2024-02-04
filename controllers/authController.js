00c078a03d629dd4157e9495b2cc674c4633eaf5
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-02-04 04:42:45 +0100

    added a comment

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,10 +36,10 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('Please provide email and password!', 400));
   }
   // 2) Check if the user exists && password is correct
-  const user = await User.findOne({ email }).select('+pass word');
+  const user = await User.findOne({ email }).select('+password');
 
-  if (!user || !(await user.correctPassword(password.user.password))) {
-    return next(new AppError('Icorrect email or password', 401));
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
   }
 
   // 3) If everything is okay, send the token to the client
