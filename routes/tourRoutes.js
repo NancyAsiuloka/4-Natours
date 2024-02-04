cc66ec1d1bf025eaf54b7b43209f1653cf6ae2ac
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-02-04 04:49:35 +0100

    added a comment

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController')
 
 // 3: ROUTES FOR TOURS
 const router = express.Router();
@@ -26,7 +27,7 @@ router
 
 router
   .route('/')
-  .get(tourController.getAllTours)
+  .get(authController,protect, tourController.getAllTours)
   .post(tourController.createTour);
 
 router
