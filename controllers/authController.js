6539dcbff0c20b88535c3e2e271090b396f16f43
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-02-04 04:29:49 +0100

    made some changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,46 +1,52 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
-const catchAsync = require('./../utils/catchAsync')
-const AppError = require('./../utils/appError')
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        passwordConfirm: req.body.passwordConfirm,
-    });
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
-    const token = jwt.sign({ id: newUser._id}, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN,
-    })
+  const token = signToken(newUser._id);
 
-    res.status(201).json({
-        status: 'Success',
-        token,
-        data: {
-            user: newUser
-        }
-    })
+  res.status(201).json({
+    status: 'Success',
+    token,
+    data: {
+      user: newUser,
+    },
+  });
 });
 
 exports.login = catchAsync(async (req, res, next) => {
-    const {email, password} = req.body;
+  const { email, password } = req.body;
 
-    //  1)Check if email and password exist
-    if(!email || !password) {
-        return next(new AppError('Please provide email and password!', 400))
-    }
-    // 2) Check if the user exists && password is correct
-    const user = await User.findOne({email}).select('+password')
-    console.log(user);
+  //  1)Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+  // 2) Check if the user exists && password is correct
+  const user = await User.findOne({ email }).select('+pass word');
 
-    // 3) If everything is okay, send the token to the client
-    const token = '';
-    res.status(200).json({
-        status: 'Success',
-        token
-    })
+  if (!user || !(await user.correctPassword(password.user.password))) {
+    return next(new AppError('Icorrect email or password', 401));
+  }
 
+  // 3) If everything is okay, send the token to the client
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'Success',
+    token,
+  });
 });
-
--- models/userModel.js
+++ models/userModel.js
@@ -1,50 +1,58 @@
 const mongoose = require('mongoose');
-const validator = require('validator')
-const bcrypt = require('bcryptjs')
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please tell us your name!'],
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: [true, 'Please provide an email'],
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // this only works on save
+      validator: function () {
+        return this.password === this.passwordConfirm;
+      },
+      message: 'Passwords are not the same!',
     },
-    email: {
-        type: String,
-        unique: true,
-        lowercase: true,
-        required: [true, 'Please provide an email'],
-        validate: [validator.isEmail, 'Please provide a valid email']
-    },
-    photo: String,
-    password: {
-        type: String,
-        required: [true, "Please provide a password"],
-        minlength: 8,
-        select: false,
-    },
-    passwordConfirm: {
-        type: String,
-        required: [true, 'Please confirm your password'],
-        validate: {
-            // this only works on save
-            validator: function() {
-                return this.password === this.passwordConfirm;
-            },
-            message: 'Passwords are not the same!'
-        }
-    }
+  },
 });
 
-userSchema.pre('save', async function(next) {
-//    Only run this fn if password was actually modified
-    if (!this.isModified('password')) return next();
+userSchema.pre('save', async function (next) {
+  //    Only run this fn if password was actually modified
+  if (!this.isModified('password')) return next();
 
-    // Hash the password with cust of 12
-    this.password = await bcrypt.hash(this.password, 12)
+  // Hash the password with cust of 12
+  this.password = await bcrypt.hash(this.password, 12);
 
-    // Delete the passwordConfirm field
-    this.passwordConfirm = undefined;
-    next();
-})
+  // Delete the passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+// comparing the password
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
