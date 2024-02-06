4db6dcce8b4d83ec4dc0d030fda63149307b2c6a
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-02-06 17:53:03 +0100

    added a comment

--- models/userModel.js
+++ models/userModel.js
@@ -36,14 +36,16 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', async function (next) {
-  //    Only run this fn if password was actually modified
   if (!this.isModified('password')) return next();
 
-  // Hash the password with cust of 12
+  // Hash the password with a cost of 12
   this.password = await bcrypt.hash(this.password, 12);
 
   // Delete the passwordConfirm field
   this.passwordConfirm = undefined;
+
+  // Update passwordChangedAt only if the document is new or modified
+  this.passwordChangedAt = Date.now() - 1000; // Subtract 1 second to ensure the token is always created after the passwordChangedAt timestamp
   next();
 });
 
@@ -55,14 +57,15 @@ userSchema.methods.correctPassword = async function (
   return await bcrypt.compare(candidatePassword, userPassword);
 };
 
-
-userSchema.methods.changePasswordAfter = function(JWTTimestamp){
-  if(this.passwordChangedAt){
-    console.log(this.passwordChangedAt, JWTTimestamp)
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+    return JWTTimestamp < changedTimestamp;
   }
 
+  // false means NOT Changed
   return false;
-}
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
