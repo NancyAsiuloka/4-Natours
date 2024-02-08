804d58d8f20363f4618e9d8b77e2ded781f376fa
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-02-08 05:44:40 +0100

    made some changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -92,3 +92,11 @@ exports.protect = catchAsync(async (req, res, next) => {
 });
 
 // Authorization is verifying if a certain user has the right to interact with a certain resource even if he's logged in
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(new AppError('You do not have permission to perform this action', 403))
+    }
+    next();
+  }
+}
