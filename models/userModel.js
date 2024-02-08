00e907bf2d5a9acd38e6e354fc01f3c564dd6119
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-02-08 05:44:52 +0100

    added a comment

--- models/userModel.js
+++ models/userModel.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     enum: ['user', 'guide', 'lead-guide',  'admin'],
-    default: 'Admin',
+    default: 'user',
   },
   password: {
     type: String,
