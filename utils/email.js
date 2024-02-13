7bf2230a44f0f200885eb5d6c79d06c471cd0afd
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-02-13 02:13:33 +0100

    added some functions

--- utils/email.js
+++ utils/email.js
@@ -1,1 +1,12 @@
 const nodemailer = require('nodemailer');
+
+const sendEmail = options => {
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth:{
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+        }
+        // Activate in gmail "less secure app" option
+    })
+}
