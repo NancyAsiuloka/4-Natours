353b8097c236ad8e2d0496771ce3310c1dd85928
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-02-14 09:28:07 +0100

    added  a comment

--- utils/email.js
+++ utils/email.js
@@ -1,12 +1,25 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = options => {
+const sendEmail = async (options) => {
+    // 1_)Create a Transpoter
     const transporter = nodemailer.createTransport({
-        service: 'Gmail',
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
         auth:{
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD
         }
-        // Activate in gmail "less secure app" option
-    })
+    });
+    // 2)Define the email options
+    const mailOptions = {
+        from: 'nancyasiuloka@gmail.com',
+        to: options.email,
+        subject: options.subject,
+        text: options.message,
+    }
+
+    // 3)
+    await transporter.sendMail(mailOptions)
 }
+
+module.exports = sendEmail
