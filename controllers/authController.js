e0e2e2f74ccd7b74800f2b6ac3103823a3e88a69
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-02-15 10:06:39 +0100

    made some changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -107,19 +107,42 @@ exports.restrictTo = (...roles) => {
 };
 
 // Forget & reset Password Functions
-exports.forgetPassword = catchAsync(async(req, res, next) => {
+exports.forgetPassword = catchAsync(async (req, res, next) => {
   // 1) Get user based on POSTED email
-  const user = await User.findOne({email: req.body.email});
-  if(!user) return next(new AppError('There is no user with email address.', 404))
+  const user = await User.findOne({ email: req.body.email });
+  if (!user)
+    return next(new AppError('There is no user with email address.', 404));
 
   // 2) Generate the random reset token
-  const resetToken =  user.createPasswordResetToken();
-  await user.save({ validateBeforeSave: false});
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
 
   // 3) Send it to user's email
-  const resetURL = ` `
+  const resetURL = `${req.protocol}://${req.get(
+    'host',
+  )}/api/v1/users/resetPassword/${resetToken}`;
 
+  const message = `forgot your password? Submit a PATCH request with your new password and
+  passwordConfirm to: ${resetURL}./nIf you didn't forget your password, please ignore this email`;
+
+  try {
+    await sendEmail({
+      email: user.email, //or req.body.email
+      subject: 'Your password reset token (valid for 10 min)',
+      message,
+    });
+
+    res.status(200).json({
+      status: 'Success',
+      message: 'Token sent to email',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(new AppError('There was an error sending the email. Try again later!', 500));
+  }
 });
 
-exports.resetPassword = (req, res, next) => {}
-
+exports.resetPassword = (req, res, next) => {};
