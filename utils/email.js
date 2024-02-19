6bed5c1b55295ff855be43bf3e021b0cc296cf8a
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-02-19 19:19:58 +0100

    changed the email address

--- utils/email.js
+++ utils/email.js
@@ -12,7 +12,7 @@ const sendEmail = async options => {
     });
     // 2)Define the email options
     const mailOptions = {
-        from: 'nancyasiuloka@gmail.com',
+        from: 'nancy@gmail.com',
         to: options.email,
         subject: options.subject,
         text: options.message,
