3ee2dbed010448867462278ed1182352b9fd1749
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-03-02 13:31:54 +0100

    logged an element to the console

--- utils/email.js
+++ utils/email.js
@@ -1,26 +1,29 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async options => {
-    // 1_)Create a Transpoter
+    // 1) Create a Transporter
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
-        auth:{
+        auth: {
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD
         }
     });
 
-    // 2)Define the email options
+    // 2) Define the email options
     const mailOptions = {
-        from: 'gidysmartodiokineh@gmail.com',
+        from: '<mama@gmail.com>',
         to: options.email,
         subject: options.subject,
         text: options.message,
     }
 
-    // 3)
+    // 3) Send the email
+    console.log('Email Options:', options);
     await transporter.sendMail(mailOptions)
+    .then(info => console.log('Email sent:', info))
+    .catch(error => console.error('Error sending email:', error));
 }
 
-module.exports = sendEmail
+module.exports = sendEmail;
