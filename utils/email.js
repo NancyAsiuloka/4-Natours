463fddf052ab68ddd485c468c3245be226b6d25c
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-03-04 14:32:21 +0100

    changed the email

--- utils/email.js
+++ utils/email.js
@@ -5,22 +5,26 @@ const sendEmail = async options => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
+        secure: false,
         auth: {
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD
-        }
+        },
+        // tls: {
+        //     ciphers:'SSLv3'
+        // }
     });
 
     // 2) Define the email options
     const mailOptions = {
-        from: '<mama@gmail.com>',
+        from: 'Asiuloka Nancy<mama@gmail.com>',
         to: options.email,
         subject: options.subject,
         text: options.message,
     }
 
     // 3) Send the email
-    console.log('Email Options:', options);
+    // console.log('Email Options:', options);
     await transporter.sendMail(mailOptions)
     .then(info => console.log('Email sent:', info))
     .catch(error => console.error('Error sending email:', error));
