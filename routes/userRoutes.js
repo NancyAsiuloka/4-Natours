159bdc660ea9c5639e477b5f9da4478ab1747b33
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-03-04 14:32:07 +0100

    added a callback function

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,27 +1,31 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
-const authController = require('./../controllers/authController')
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-
 // ROUTES FOR USERS
-router.post('/signup', authController.signup)
-router.post('/login', authController.login)
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
 
-router.post('/forgotPassword', authController.forgetPassword)
-router.patch('/resetPassword/:token', authController.resetPassword)
+router.post('/forgotPassword', authController.forgetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword
+);
 
 router
-.route('/')
-.get(userController.getAllUsers)
-.post(userController.createUser);
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
 
 router
-.route('/:id')
-.get(userController.getUser)
-.patch(userController.updateUser)
-.delete(userController.deleteUser);
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
