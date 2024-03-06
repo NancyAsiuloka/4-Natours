ab02cbb52488a69d5bf280a0bbef58f3a65e401d
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-03-06 12:50:08 +0100

    made some changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,23 @@
 const User = require('./../models/userModel');
-const catchAsync = require('./../utils/catchAsync')
-const AppError = require('./../utils/appError')
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {}
+  Object.keys(obj).forEach(el => {
+    if(allowedFields.includes(el)) newObj[el] =obj[el]
+  });
+return newObj;
+}
+
+exports.deleteMe = catchAsync(async(req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, {active: false})
+
+  res.status(204).json({
+    status: 'Success',
+    data: null
+  })
+})
 
 // ROUTE HANDLER FOR USERS
 exports.getAllUsers = catchAsync(async (req, res, next) => {
@@ -19,62 +35,84 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.updateMe = (req, res, next) => {
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password update. Please use /updateMyPassword',
+        400,
+      ),
+    );
+  }
+// 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
 
-}
+  // 2) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
 
 exports.getUser = (req, res) => {
-    const id = req.params.id * 1; // Convert the parameter to a number
-    console.log('Searching for user with ID:', id);
-    const user = users.find((user) => user.id === id);
-    console.log(user);
+  const id = req.params.id * 1; // Convert the parameter to a number
+  console.log('Searching for user with ID:', id);
+  const user = users.find((user) => user.id === id);
+  console.log(user);
 
-    if (!user) {
-        res.status(404).json({
-            status: 'error',
-            message: 'User not found'
-        });
-    } else {
-        res.status(200).json({
-            status: 'success',
-            data: {
-                user: user
-            }
-        });
-    }
+  if (!user) {
+    res.status(404).json({
+      status: 'error',
+      message: 'User not found',
+    });
+  } else {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user: user,
+      },
+    });
+  }
 };
 
-
 exports.createUser = (req, res) => {
-    const newId = users[users.length - 1].id + 1;
-    const newUser = Object.assign({ id: newId }, req.body);
+  const newId = users[users.length - 1].id + 1;
+  const newUser = Object.assign({ id: newId }, req.body);
 
-    users.push(newUser);
+  users.push(newUser);
 
-    fs.writeFile(
-      `${__dirname}/dev-data/data/users.json`,
-      JSON.stringify(users),
-      (err) => {
-        res.status(201).json({
-          status: 'success',
-          data: {
-            user: newUser,
-          },
-        });
-      }
-    );
+  fs.writeFile(
+    `${__dirname}/dev-data/data/users.json`,
+    JSON.stringify(users),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          user: newUser,
+        },
+      });
+    },
+  );
 };
 
 exports.updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
-    });
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
 };
 
 exports.deleteUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
-    });
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
 };
