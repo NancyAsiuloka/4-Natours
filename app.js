8cf850f60c1ad9532ea86271288c8e1f957be01c
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-03-08 06:33:15 +0100

    made some changes

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -10,12 +14,16 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) GLOBAL: First middlewares
+    // Set security HTTP headers
+    app.use(helmet())
+
 // using this middleware to handle the post request
+// Development Loggin
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-// Limiting requests from same IP
+// Limiting requests from same API
 const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
@@ -23,9 +31,28 @@ const limiter = rateLimit({
 })
 app.use('/api', limiter);
 
+// Body-parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb'}));
 
-app.use(express.json());
-// how to serve static files from a folder & not from a route
+// Data Sanitization agains NoSQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp({
+    whitelist: [
+        'duration',
+        'ratingsQuantity',
+        'ratingsAverage',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+    ]
+}));
+
+// Serving static files from a folder & not from a route
 app.use(express.static(`${__dirname}/public`));
 
 
@@ -35,6 +62,7 @@ app.use((req, res, next) => {
     next();
 });
 
+// Test Middleware
 // creating another middleware to manipulate the request object
 app.use((req, res, next) => {
     //adding the current time to the request
