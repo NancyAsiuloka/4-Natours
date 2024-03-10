91cc2632ab08fdfc8808eee2248abd1369b640b4
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-03-10 01:36:50 +0100

    made some changes

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
-const User = require('../models/userModel')
+// const User = require('../models/userModel')
 
 const tourSchema = new mongoose.Schema(
   {
