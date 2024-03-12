fe5c823a6faad387b8d51efb6a72c20ca9e3be30
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-03-12 18:11:00 +0100

    made some changes

---
+++ models/reviewModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review must be filled'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
