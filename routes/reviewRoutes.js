0050d82cc7727f3b1c2a24c9da5a7ad613100b10
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-03-12 18:43:23 +0100

    made some changes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const reviewController = require('./../controllers/reviewController')
+const reviewController = require('./../controllers/reviewController');
+const authController = require('./../controllers/authController');
 
 // 3: ROUTES FOR TOURS
 const router = express.Router();
 
 router
   .route('/')
-  .get(authController.protect, reviewController.getAllReviews)
-  .post(reviewController.createReview);
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview,
+  );
 
-
-  module.exports = router;
+module.exports = router;
