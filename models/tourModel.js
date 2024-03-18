bf03973ac47275c9c42181ef8b4a4d6881438883
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-03-18 01:11:05 +0100

    implemented virtual population

--- models/tourModel.js
+++ models/tourModel.js
@@ -120,6 +120,12 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id',
+})
+
 // //   DOCUMENT MIDDLEWARE: runs before .save() and .create() command
 tourSchema.pre('save', function (next) {
     //this points to the current doc being saved
