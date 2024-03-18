848aaaa7db6c01609bb731abf30d859e838669a7
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-03-18 01:05:48 +0100

    imported a module

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -32,17 +32,17 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
-// reviewSchema.pre(/^find/, function (next) {
-//   this.populate({
-//     path:'tour',
-//     select: 'name'
-//   }).populate({
-//     path: 'user',
-//     select: 'name photo'
-//   });
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path:'tour',
+    select: 'name'
+  }).populate({
+    path: 'user',
+    select: 'name photo'
+  });
 
-//   next();
-// });
+  next();
+});
 
 const Review = mongoose.model('Review', reviewSchema);
 module.exports = Review;
