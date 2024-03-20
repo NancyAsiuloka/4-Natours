e9ba2e918bcf71b6e5f4e1bbcb304bc8921723c8
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-03-20 01:35:35 +0100

    added a comment

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -44,7 +44,7 @@ router
   .route('/:tourId/reviews')
   .post(
     authController.protect,
-    authController.restrictTo('users'),
+    authController.restrictTo('user'),
     reviewController.createReview,
   );
 
