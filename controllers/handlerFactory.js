f886e9c5a94ddfdc4a2d762f9a3bf24366a8a397
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-03-30 14:07:50 +0100

    added a function

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,32 @@
+exports.deleteOne = Model => catchAsync(async (req, res, next) => {
+    const tour = await Tour.deleteOne(req.params.id, req.body);
+
+  if(!tour){
+    return next(new AppError('No tour found with that ID', 404))
+  }
+
+    // respond status code
+    res.status(204).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+});
+
+// Handling the delete request
+exports.deleteTour = catchAsync(async (req, res, next) => {
+    const tour = await Tour.deleteOne(req.params.id, req.body);
+
+  if(!tour){
+    return next(new AppError('No tour found with that ID', 404))
+  }
+
+    // respond status code
+    res.status(204).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+});
