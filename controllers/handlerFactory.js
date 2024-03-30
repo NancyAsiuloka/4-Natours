9c38a609f7ba4d476f81546697aa1df21121a307
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-03-30 14:09:36 +0100

    added a comment

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -2,7 +2,7 @@ exports.deleteOne = Model => catchAsync(async (req, res, next) => {
     const doc = await Model.deleteOne(req.params.id, req.body);
 
   if(!doc){
-    return next(new AppError('No tour found with that ID', 404))
+    return next(new AppError('No document found with that ID', 404))
   }
 
     // respond status code
@@ -14,19 +14,3 @@ exports.deleteOne = Model => catchAsync(async (req, res, next) => {
     });
 });
 
-// Handling the delete request
-exports.deleteTour = catchAsync(async (req, res, next) => {
-    const tour = await Tour.deleteOne(req.params.id, req.body);
-
-  if(!tour){
-    return next(new AppError('No tour found with that ID', 404))
-  }
-
-    // respond status code
-    res.status(204).json({
-      status: 'success',
-      data: {
-        tour,
-      },
-    });
-});
