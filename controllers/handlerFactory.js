1103620e93c0b33558138d0e5068642609a947e1
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-04-08 11:10:54 +0100

    added a comment

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -3,7 +3,7 @@ const AppError = require('../utils/appError')
 
 exports.deleteOne = Model => catchAsync(async (req, res, next) => {
     const doc = await Model.deleteOne({id: req.params.id});
-
+    console.log(doc)
   if(!doc){
     return next(new AppError('No document found with that ID', 404))
   }
