2e0fbcde64f9f0fd9f87c203495fd65ef93850cb
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-07-07 17:09:24 +0100

    added a comment

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -82,6 +82,7 @@ reviewSchema.pre(/^findOneAnd/, async function (next) {
   // Fetch the document to be updated or deleted using the ID from the query
   this.r = await Review.findById(this.getQuery()._id);
   console.log(this.r);
+
   next();
 });
 
