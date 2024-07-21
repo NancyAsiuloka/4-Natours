d0841fb951a99d8b49078b6a8b3ffce962ff5225
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-07-22 00:26:31 +0100

    added a comment

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,7 +17,12 @@ router.patch(
   authController.updatePassword,
 );
 
-router.get('/me', authController.protect, userController.getMe, userController.getUser)
+router.get(
+  '/me',
+  authController.protect,
+  userController.getMe,
+  userController.getUser,
+);
 router.patch('/updateMe', authController.protect, userController.updateMe);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
@@ -31,6 +36,4 @@ router
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
-;
-
 module.exports = router;
