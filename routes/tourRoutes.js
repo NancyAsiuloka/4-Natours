bbae57a023ea1d55f4739bfc7db8002ef19aee50
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-08-03 00:57:00 +0100

    made some changes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -17,9 +17,14 @@ router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
-
 router.route('/tour-stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.getMonthlyPlan);
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.getMonthlyPlan,
+  );
 
 router
   .route('/')
