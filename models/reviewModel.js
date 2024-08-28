d5aec90e718e69bad2d6722e18e96dc681f6e43d
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-08-28 01:17:49 +0100

    made some changes

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -66,7 +66,7 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
   console.log(stats);
 
   // Persist is to update the tour on the avg & n rating
-  Tour.findByIdAndUpdate(tourId, {
+  await Tour.findByIdAndUpdate(tourId, {
     ratingsQuantity: stats[0].nRating,
     ratingsAverage: stats[0].avgRating,
   })
