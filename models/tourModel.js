57a027e476a752cd20e3e92fd109e9beed843584
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-09-23 09:11:12 +0100

    added a comment

--- models/tourModel.js
+++ models/tourModel.js
@@ -179,12 +179,12 @@ tourSchema.post(/^find/, function (docs, next) {
 });
 
 // AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  console.log(this);
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//   console.log(this.pipeline());
 
-  next();
-});
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
