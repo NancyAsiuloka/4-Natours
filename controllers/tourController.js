21de4ee4fbeab0c6a02add58dfd5c3bd1b75404b
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-09-23 09:28:57 +0100

    added a multiplier to the aggregation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -140,6 +140,8 @@ exports.getDistances = catchAsync(async (req, res, next) => {
   const { latlng, unit } = req.params;
   const [lat, lng] = latlng.split(',');
 
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
   if (!lat || !lng) {
     next(
       new AppError(
@@ -156,7 +158,14 @@ exports.getDistances = catchAsync(async (req, res, next) => {
           type: 'Point',
           coordinates: [lng * 1, lat * 1]
         },
-        distanceField: 'distance'
+        distanceField: 'distance',
+        distanceMultiplier: multiplier,
+      }
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1
       }
     }
   ])
