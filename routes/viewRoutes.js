5d564c6f5cd04e926f92c3e1effbdc6a52ecc7be
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-09-29 10:22:45 +0100

    created route for the tour detail page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,7 +4,7 @@ const  viewController = require('../controllers/viewController')
 const router = express.Router();
 
 router.get('/', viewController.getOverview);
-router.get('/tour', viewController.getTour);
+router.get('/tour/:slug', viewController.getTour);
 
 
 module.exports = router;
