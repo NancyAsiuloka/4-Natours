d0618d915cdfbe16460ea734d067793716bdcbd9
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-09-29 09:19:36 +0100

    installed a package

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,24 +1,29 @@
-const Tour = require('../models/tourModel')
-const catchAsync = require('../utils/catchAsync')
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+const dayjs = require('dayjs');
 
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
 
-exports.getOverview = catchAsync( async(req, res) => {
-    // 1) Get tour data from collection
-    const tours = await Tour.find();
+  // 2) Format the tour start dates using Day.js
+  const formattedTours = tours.map((tour) => ({
+    ...tour._doc,
+    formattedStartDate: dayjs(tour.startDates[0]).format('MMM YYYY'),
+  }));
 
-    // 2) Build template
+  // 2) Build template
 
-    // 3) Render that template using the tour data from 1)
+  // 3) Render that template using the tour data from 1)
 
-    res.status(200).render('overview', {
-        title: 'All Tours',
-        tours,
-    });
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours: formattedTours,
+  });
 });
 
-exports.getTour = catchAsync( async(req, res) => {
-    res.status(200).render('tour', {
-        title: 'The Forest Hiker Tour',
-    });
-})
-
+exports.getTour = catchAsync(async (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
+});
