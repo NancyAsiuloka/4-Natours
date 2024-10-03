6551bb08a1395b963f78f7afffc61b98ec74125e
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-10-03 11:06:05 +0100

    made some changes

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -3,3 +3,10 @@ console.log('Hello from the client side')
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 
 console.log(locations)
+
+const map = locations.map('map').setView([51.505, -0.09], 13);
+
+locations.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
