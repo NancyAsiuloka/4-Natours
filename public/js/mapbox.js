109ad492614e1e54d5df276841f01d86d3c0e4b2
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-10-04 09:20:55 +0100

    made some changes

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,12 +1,20 @@
-console.log('Hello from the client side')
+console.log('Hello from the client side');
 
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 
-console.log(locations)
+console.log(locations);
 
-const map = locations.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([51.505, -0.09], 13);
 
-locations.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// Set the OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+// Loop through the locations and add markers to the map
+locations.forEach(location => {
+  L.marker([location.lat, location.lng])
+    .addTo(map)
+    .bindPopup(`<p>${location.description}</p>`);
+});
