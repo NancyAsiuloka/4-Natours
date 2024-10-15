4ee6ae3434e1962ec8cb0991d2f65581776acb6a
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-10-15 11:49:04 +0100

    added a comment

--- models/tourModel.js
+++ models/tourModel.js
@@ -133,7 +133,6 @@ tourSchema.virtual('reviews', {
   localField: '_id',
 });
 
-
 // //   DOCUMENT MIDDLEWARE: runs before .save() and .create() command
 tourSchema.pre('save', function (next) {
   //this points to the current doc being saved
