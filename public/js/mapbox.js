060d47948249cb43d861907d5fa2dafa9f5dd423
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-10-26 14:09:18 +0100

    added some methods to the mapbox

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -6,7 +6,6 @@ console.log(locations);
 
 const map = L.map('map').setView([51.505, -0.09], 13);
 
-// Set the OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
