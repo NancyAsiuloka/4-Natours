aca8d1bdcbfa882bbbc213f41e206c310c9de42c
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-10-30 14:10:39 +0100

    added some methods to the mapbox

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,6 +1,6 @@
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 
-console.log(locations);
+// console.log(locations);
 
 const map = L.map('map').setView([51.505, -0.09], 13);
 
