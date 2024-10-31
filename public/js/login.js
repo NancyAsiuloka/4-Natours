cf2bdf94f03e54dc3b5a73e3b06b6655db904a96
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-10-31 15:55:38 +0100

    logged an element to the console

--- public/js/login.js
+++ public/js/login.js
@@ -3,7 +3,7 @@ const login = async(email, password ) => {
     try {
     const res = await axios({
         method: 'POST',
-        url: 'http://localhost:6000/api/v1/users/login',
+        url: 'http://localhost:3000/api/v1/users/login',
         data: {
             email,
             password
@@ -11,7 +11,7 @@ const login = async(email, password ) => {
     });
     console.log(res)
   } catch (err){
-    console.log(err)
+    console.log(err.response.data)
   }
 };
 
@@ -19,5 +19,5 @@ document.querySelector('.form').addEventListener('submit', e => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    login({email, password})
+    login(email, password)
 })
