e6ff86ae08a283f6355ccf1772f26491f31c463e
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-10-31 15:06:47 +0100

    building the login template

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -22,7 +22,7 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
     // 1) Get the data, for the requested tour (including reviews and guides)
     const tour = await Tour.findOne({slug: req.params.slug}).populate({
         path: "reviews",
@@ -37,3 +37,9 @@ exports.getTour = catchAsync(async (req, res) => {
     tour,
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    tiltle: 'Log into your account'
+  })
+}
