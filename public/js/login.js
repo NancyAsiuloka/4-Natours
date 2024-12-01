bdd57623108c41808daf5ae51fe370ab913093b8
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-12-01 23:20:11 +0100

    added node modules to gitignore file

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { showAlert } from "./alert";
 
 export const login = async(email, password ) => {
     try {
@@ -13,13 +14,24 @@ export const login = async(email, password ) => {
 
 
     if(res.data.status === 'Success'){
-      alert('Logged in successfully!')
+      showAlert('Success', 'Logged in successfully!')
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
     }
   } catch (err){
-    alert(err.response.data.message)
+    showAlert('error', err.response.data.message)
   }
 };
 
+export const logout = async() => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://127.0.0.1:3000/api/v1/users/logout',
+    })
+    if(res.data.status === 'Success')location.reload(true);
+  } catch(err){
+    showAlert('error', 'Error logging out! try again.')
+  }
+}
