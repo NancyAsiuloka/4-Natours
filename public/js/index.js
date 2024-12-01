68bb63ca49234cf39b4255e873fe69e3662773e8
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-12-01 23:20:01 +0100

    created a new file

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,20 @@
 import '@babel/polyfill';
-import { login } from "./login";
+import { login, logout } from './login';
 
-document.querySelector('.form').addEventListener('submit', e => {
+// DOM ELEMENTS
+const loginForm = document.querySelector('.form');
+const logOutBtn = document.querySelector('.nav__el--logout')
+
+// VALUES
+
+// DELEGATION
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    login(email, password)
-});
+    login(email, password);
+  });
+}
+
+if(logOutBtn) logOutBtn.addEventListener('click', logout)
