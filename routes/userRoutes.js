88e366a564f75db3d1b8d2a2b013ce49207a73a0
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-12-03 23:21:51 +0100

    made some changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 // ROUTES FOR USERS
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.get('/logout', authController.logout);
 
 router.post('/forgotPassword', authController.forgetPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
