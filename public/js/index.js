5b9ce9ef70cd26b0e9861fbae94b7cae9c1fe641
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-12-05 16:23:57 +0100

    made some changes

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
 import '@babel/polyfill';
 import { login, logout } from './login';
+import { updateData } from './updateSettings';
 
 // DOM ELEMENTS
-const loginForm = document.querySelector('.form');
-const logOutBtn = document.querySelector('.nav__el--logout')
+const loginForm = document.querySelector('.form--login');
+const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 // VALUES
 
@@ -17,4 +19,12 @@ if (loginForm) {
   });
 }
 
-if(logOutBtn) logOutBtn.addEventListener('click', logout)
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm)
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateData(name, email)
+  });
