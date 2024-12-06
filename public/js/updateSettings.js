bba2a9d519e2a040f672df27751a854fa1094eda
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-12-06 10:48:39 +0100

    made some changes

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,20 +1,22 @@
 import axios from 'axios';
 import { showAlert } from './alert';
 
-export const updateData = async(name, email) => {
+// type is either 'password' or 'data'
+export const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
+        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
     const res = await axios({
-        method: 'PATCH',
-        url: "http://127.0.0.1:3000/api/v1/users/updateMe",
-        data: {
-            name,
-            email,
-        }
-    })
-    if (res.data.status === 'Success'){
-        showAlert('Success', 'Data updated successfully.')
+      method: 'PATCH',
+      url,
+      data,
+    });
+    if (res.data.status === 'Success') {
+      showAlert('Success', `${type.toUpperCase()}Data updated successfully.`);
     }
-  } catch (err){
-    showAlert('error', err.response.data.message)
+  } catch (err) {
+    showAlert('error', err.response.data.message);
   }
-}
+};
