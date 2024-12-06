b49a9bd142c6e6cbecf2da405c7d049f410f480b
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-12-06 12:20:49 +0100

    added file filter configuration for multer

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const factory = require('./handlerFactory')
+const factory = require('./handlerFactory');
 
 const multerStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -11,43 +11,53 @@ const multerStorage = multer.diskStorage({
   filename: (req, file, cb) => {
     // user-274889feqwhb98-23447.jpeg
     const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
 
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images', 400), false);
   }
-})
+};
 
-const upload = multer({ dest: 'public/img/users'});
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+});
 exports.uploadUserPhoto = upload.single('photo');
 
 const filterObj = (obj, ...allowedFields) => {
-  const newObj = {}
-  Object.keys(obj).forEach(el => {
-    if(allowedFields.includes(el)) newObj[el] =obj[el]
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
   });
 
-return newObj;
-
-}
+  return newObj;
+};
 
 exports.getMe = (req, res, next) => {
   req.params.id = req.user.id;
 
   next();
-}
+};
 
-exports.deleteMe = catchAsync(async(req, res, next) => {
-  await User.findByIdAndUpdate(req.user.id, {active: false})
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
 
   res.status(204).json({
     status: 'Success',
-    data: null
-  })
-})
+    data: null,
+  });
+});
 
 // ROUTE HANDLER FOR USERS
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file)
-  console.log(req.body)
+  console.log(req.file);
+  console.log(req.body);
 
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
@@ -59,7 +69,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     );
   }
 
-// 2) Filtered out unwanted fields names that are not allowed to be updated
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
 
   // 2) Update user document
@@ -71,12 +81,10 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: 'success',
     data: {
-      user: updatedUser
-    }
+      user: updatedUser,
+    },
   });
 });
-
-
 
 exports.createUser = (req, res) => {
   res.status(500).json({
@@ -85,10 +93,10 @@ exports.createUser = (req, res) => {
   });
 };
 
-exports.getAllUsers = factory.getAll(User)
+exports.getAllUsers = factory.getAll(User);
 
-exports.getUser = factory.getOne(User)
+exports.getUser = factory.getOne(User);
 // Do NOT update passwords with this!
-exports.updateUser = factory.updateOne(User)
+exports.updateUser = factory.updateOne(User);
 
-exports.deleteUser = factory.deleteOne(User)
+exports.deleteUser = factory.deleteOne(User);
