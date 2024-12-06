2bc2e9b73e02ff5558e9b74f7f12e8640bcdff7b
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-12-06 10:49:43 +0100

    added a comment

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.use((req, res, next) => {
 // creating another middleware to manipulate the request object
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.cookies)
+  // console.log(req.cookies)
   next();
 });
 
