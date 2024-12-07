21e69f8134f1166bfdd311f440594c1aa14ad6c5
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-12-07 12:21:18 +0100

    creating email template using pug to send emails

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,58 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+
+// new Email(user, url).sendWelcome();
+module.exports = class Email {
+    constructor(user, url){
+        this.to = user.email;
+        this.firstName = user.name.split(' ')[0];
+        this.url = url
+        this.from = `Asiuloka Nancy<${process.env.EMAIL_FROM}>`;
+    }
+
+    createTransport(){
+        if(process.env.NODE_ENV === 'production'){
+            // sendgrid
+            return 1;
+        }
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            secure: false,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        });
+    }
+
+    // send the actual email
+    send(template, subject){
+        // 1) Render HTML based on a pug template
+        const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`)
+
+        // 2) Define the email options
+        const mailOptions = {
+            from: 'Asiuloka Nancy<mama@gmail.com>',
+            to: options.email,
+            subject: options.subject,
+            text: options.message,
+        }
+
+        // 3) Create a transport and send email
+
+
+
+    }
+
+    sendWelcome(){
+        this.send('welcome', 'Welcome to the Natours Family!')
+    }
+}
 
 const sendEmail = async options => {
-    // 1) Create a Transporter
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        secure: false,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
-        },
-        // tls: {
-        //     ciphers:'SSLv3'
-        // }
-    });
 
     // 2) Define the email options
-    const mailOptions = {
-        from: 'Asiuloka Nancy<mama@gmail.com>',
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-    }
 
     // 3) Send the email
     // console.log('Email Options:', options);
