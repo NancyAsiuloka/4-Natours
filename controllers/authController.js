3a2c6f7ccc5d47b1ee9cb318c459c98864e118d6
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-12-08 12:08:59 +0100

    made some changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const sendEmail = require('./../utils/email');
+const Email = require('./../utils/email');
 const { AsyncResource } = require('async_hooks');
 
 const signToken = (id) => {
@@ -45,6 +45,11 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
     role: req.body.role,
   });
+
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  console.log(url)
+
+  await new Email(newUser, url).sendWelcome()
 
   createSendToken(newUser, 201, res);
 });
@@ -190,11 +195,11 @@ exports.forgetPassword = catchAsync(async (req, res, next) => {
   passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email`;
 
   try {
-    await sendEmail({
-      email: user.email, //or req.body.email
-      subject: 'Your password reset token (valid for 10 min)',
-      message,
-    });
+    // await sendEmail({
+    //   email: user.email, //or req.body.email
+    //   subject: 'Your password reset token (valid for 10 min)',
+    //   message,
+    // });
 
     res.status(200).json({
       status: 'Success',
