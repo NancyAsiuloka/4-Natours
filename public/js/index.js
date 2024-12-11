3e08acc8aeead9b11cb470da684527b4df6d92db
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-12-11 09:45:37 +0100

    added a comment

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,14 @@
 import '@babel/polyfill';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
+import { bookTour } from './stripe'
 
 // DOM ELEMENTS
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
 // VALUES
 
@@ -52,3 +54,11 @@ if (userPasswordForm)
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
   });
+
+
+  if(bookBtn)
+    bookBtn.addEventListener('click', e => {
+      e.target.textContent = 'Processing...';
+      const { tourId } = e.target.dataset;
+      bookTour(tourId)
+  })
