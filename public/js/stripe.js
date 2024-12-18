a746b1e64f8b0f326489fd9fa336e7ef7040ecfe
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-12-18 09:38:30 +0100

    made some changes

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,14 +1,20 @@
-import axios from 'axios';
-const stripe = Stripe(
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(
   'pk_test_51QUAXeAcPH5FkzkovQjueqTwBIisrak88FJVS5XO9O8JTvtGJ2qZPjdF78gJDBgVfT2tOg1GxKqGgwWG30Kqufq2001ZfII7mN',
 );
 
 export const bookTour = async (tourId) => {
+  try {
   // 1) Get checkout session from API
-  const session = await axios(
+  const session = await axios.get(
     `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`,
   );
-  console.log(session)
+  console.log(session);
 
   // 2) Create checkout form + charge credit card
+  } catch (err){
+    console.log(err);
+
+  }
 };
