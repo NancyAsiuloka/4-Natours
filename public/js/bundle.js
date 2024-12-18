407c56e1d9505380804fbcfa7a39692b7f78b414
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-12-18 09:19:45 +0100

    made some changes

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -13024,7 +13024,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61338" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54907" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
