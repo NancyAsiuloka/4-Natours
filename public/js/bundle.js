84dcab56120357c737c9e533215f34ae11392d8c
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-12-19 21:26:59 +0100

    imported axios

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -13212,7 +13212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59789" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61114" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
